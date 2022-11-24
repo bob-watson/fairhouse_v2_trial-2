05a442de28aca76edb73f76c3cc6c1544ede74e8
Bob Watson <bobwatson@google.com>
2022-11-23 16:25:46 -0800

    lesson-02-step-4

--- Lesson-02/fairhouse/src/app/home/home.component.ts
+++ Lesson-02/fairhouse/src/app/home/home.component.ts
@@ -6,11 +6,30 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      home works!
-    </p>
-  `,
+  <section>
+    <input type="text" placeholder="Filter by city">
+    <button type="button">Search</button>
+  </section>
+  <section class="results">
+  </section>
+`,
   styles: [
+    `input, button {
+      font-size: 18pt;
+      width: 60%;
+      padding: 8px;
+      border-radius: 10px;
+      border: solid .05em #4468e8
+    }`,
+    `button {
+      margin-left: .2em;
+      width: auto;
+    }`,
+    `.results {
+      display: flex;
+      flex-wrap: wrap;
+      margin-top: 4em;
+    }`
   ]
 })
 export class HomeComponent {
