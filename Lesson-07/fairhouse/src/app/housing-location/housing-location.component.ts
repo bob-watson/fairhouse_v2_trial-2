f7d71e88d7e38246442f28a839207c7a2f789149
Bob Watson <bobwatson@google.com>
2022-12-01 09:01:12 -0800

    lesson-07-step-2

--- Lesson-07/fairhouse/src/app/housing-location/housing-location.component.ts
+++ Lesson-07/fairhouse/src/app/housing-location/housing-location.component.ts
@@ -7,13 +7,30 @@ import { HousingLocation } from '../housing-location';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <h1>{{this.housingLocation.name}}</h1>
-    <p><code>{{this.housingLocation | json}}</code></p>
-    <p>
-      housing-location works!
-    </p>
+    <img class="listing-photo">
+    <h1 class="listing-heading">{{housingLocation.name}}</h1>
+    <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
   `,
   styles: [
+    `.listing {
+      margin: .5em 2em 4em 0;
+    }`,
+    `.listing-heading {
+      font-size: 18pt;
+      width: 98%;
+      height: 60px;
+    }`,
+
+    `.listing-location {
+      color: gray;
+    }`,
+
+    `.listing-photo {
+      height: 300px;
+      width: 300px;
+      object-fit: cover;
+      border-radius: 10px;
+    }`
   ]
 })
 export class HousingLocationComponent implements OnInit {
